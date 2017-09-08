e7c0e6aafae00791864304f59a3082dbc70169c9
sergio <sergio.sousa@gmail.com>
2017-09-08 18:59:31 -0300

    Alteracao 1

--- app/src/main/java/com/example/francisco/calculojurospagamento/MainMenu.kt
+++ app/src/main/java/com/example/francisco/calculojurospagamento/MainMenu.kt
@@ -1,12 +1,14 @@
 package com.example.francisco.calculojurospagamento
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainMenu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        // ok
     }
 }
