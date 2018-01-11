26e88a63d9614cd32ec2bb677be35a231c971f99
fcoedn <fcoedn@gmail.com>
2018-01-10 22:04:45 -0300

    Casa 001

--- app/src/main/java/com/example/francisco/calculojurospagamento/MainCalculo.kt
+++ app/src/main/java/com/example/francisco/calculojurospagamento/MainCalculo.kt
@@ -2,11 +2,19 @@ package com.example.francisco.calculojurospagamento
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import java.text.SimpleDateFormat
 
 class MainCalculo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_calculo)
+
+        val textView: TextView = findViewById(R.id.TxDtPagamento)
+        textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+
+
     }
 }
