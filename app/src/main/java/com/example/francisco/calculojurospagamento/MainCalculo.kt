fe65e5fb78f5a5e1a48b2f9d5849ee625f4ffd72
sergio <sergio.sousa@gmail.com>
2018-01-12 18:06:40 -0300

    Trabalho 10001

--- app/src/main/java/com/example/francisco/calculojurospagamento/MainCalculo.kt
+++ app/src/main/java/com/example/francisco/calculojurospagamento/MainCalculo.kt
@@ -1,10 +1,17 @@
 package com.example.francisco.calculojurospagamento
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main_calculo.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -58,6 +65,28 @@ class MainCalculo : AppCompatActivity() {
                     cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+        // Calculo dos Juros
+        val btnCalcular: Button = findViewById(R.id.btnCalcular)
+        //btnCalcular.text = "Rogerio"
+        btnCalcular.setOnClickListener {
+            val vlraPagar: EditText = findViewById(R.id.VlrPagar)
+            val a: String = vlraPagar.text.toString()
+
+            if (a.isEmpty()) {
+               Toast.makeText(applicationContext,"Valor Incorreto", Toast.LENGTH_SHORT).show()
+            } else {
+                val b = a.toInt() * 2
+                //result.setText(e.toString())
+
+                AlertDialog.Builder(this)
+                        .setTitle("Juros")
+                        .setMessage("Valor $b")
+                        .setPositiveButton("Ok", { dialog, which -> }).show()
+                val resultadoIntent =  Intent(this, MainResultado::class.java)
+                startActivity(resultadoIntent);
+            }
         }
     }
 
