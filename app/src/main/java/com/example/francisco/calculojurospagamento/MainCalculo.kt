f7478988438593b4784071b6720ec97d8ee1a74c
sergio <sergio.sousa@gmail.com>
2018-01-12 16:45:26 -0300

    Trabalho 1000

--- app/src/main/java/com/example/francisco/calculojurospagamento/MainCalculo.kt
+++ app/src/main/java/com/example/francisco/calculojurospagamento/MainCalculo.kt
@@ -37,61 +37,82 @@ class MainCalculo : AppCompatActivity() {
                     cal.get(Calendar.DAY_OF_MONTH)).show()
         }
 
-        val textViewV: TextView = findViewById(R.id.DtVencimento)
-        textViewV.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
-    }
+        // Data do Vencimento
+        val DtVencimento: TextView = findViewById(R.id.DtVencimento)
+        DtVencimento.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+        DtVencimento.setOnClickListener {
+            var cal = Calendar.getInstance()
 
-    fun infdtpagamento1 (view: View) {
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-        val textView: TextView = findViewById(R.id.DtPagamento)
-        //textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+                val myFormat = "dd.MM.yyyy" // mention the format you need
+                val sdf = SimpleDateFormat(myFormat, Locale.US)
+                DtVencimento.text = sdf.format(cal.time)
 
-        var cal = Calendar.getInstance()
+            }
 
-        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            cal.set(Calendar.YEAR, year)
-            cal.set(Calendar.MONTH, monthOfYear)
-            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
-            val myFormat = "dd.MM.yyyy" // mention the format you need
-            val sdf = SimpleDateFormat(myFormat, Locale.US)
-            textView.text = sdf.format(cal.time)
-
-        }
-
-       textView.setOnClickListener {
             DatePickerDialog(this@MainCalculo, dateSetListener,
                     cal.get(Calendar.YEAR),
                     cal.get(Calendar.MONTH),
                     cal.get(Calendar.DAY_OF_MONTH)).show()
         }
-
     }
 
-    fun infdtvencimento (view: View) {
+    // Antes chamado pelo activity_main_calculo.xml
+    //fun infdtpagamento1 (view: View) {
 
-        val textView: TextView = findViewById(R.id.DtPagamento)
+    //    val textView: TextView = findViewById(R.id.DtPagamento)
         //textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
 
-        var cal = Calendar.getInstance()
+    //    var cal = Calendar.getInstance()
 
-        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            cal.set(Calendar.YEAR, year)
-            cal.set(Calendar.MONTH, monthOfYear)
-            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+     //   val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+     //       cal.set(Calendar.YEAR, year)
+     //       cal.set(Calendar.MONTH, monthOfYear)
+     //       cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-            val myFormat = "dd.MM.yyyy" // mention the format you need
-            val sdf = SimpleDateFormat(myFormat, Locale.US)
-            textView.text = sdf.format(cal.time)
+     //       val myFormat = "dd.MM.yyyy" // mention the format you need
+     //       val sdf = SimpleDateFormat(myFormat, Locale.US)
+     //       textView.text = sdf.format(cal.time)
 
-        }
+     //   }
+
+     //  textView.setOnClickListener {
+     //       DatePickerDialog(this@MainCalculo, dateSetListener,
+     //               cal.get(Calendar.YEAR),
+     //               cal.get(Calendar.MONTH),
+     //               cal.get(Calendar.DAY_OF_MONTH)).show()
+     //   }
+
+    //}
+
+    //fun infdtvencimento (view: View) {
+
+     //   val textView: TextView = findViewById(R.id.DtPagamento)
+        //textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+     //   var cal = Calendar.getInstance()
+
+     //   val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+     //       cal.set(Calendar.YEAR, year)
+     //       cal.set(Calendar.MONTH, monthOfYear)
+     //       cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+     //       val myFormat = "dd.MM.yyyy" // mention the format you need
+    //        val sdf = SimpleDateFormat(myFormat, Locale.US)
+     //       textView.text = sdf.format(cal.time)
+
+     //   }
 
         //  textView.setOnClickListener {
-        DatePickerDialog(this@MainCalculo, dateSetListener,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)).show()
+     //   DatePickerDialog(this@MainCalculo, dateSetListener,
+     //           cal.get(Calendar.YEAR),
+     //           cal.get(Calendar.MONTH),
+     //           cal.get(Calendar.DAY_OF_MONTH)).show()
         // }
 
-    }
+    //}
 }
