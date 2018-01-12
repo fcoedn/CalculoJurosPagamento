9d9ecb0e8d102db87eecead42559c7c30904d3fc
fcoedn <fcoedn@gmail.com>
2018-01-11 21:50:47 -0300

    Casa 003

--- app/src/main/java/com/example/francisco/calculojurospagamento/MainMenu.kt
+++ app/src/main/java/com/example/francisco/calculojurospagamento/MainMenu.kt
@@ -5,59 +5,69 @@ import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 
-class MainMenu : AppCompatActivity() {
+class MainMenu : AppCompatActivity()   {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
+        val btnCalcular: Button = findViewById(R.id.btnCalcular)
+        btnCalcular.text = "Calcular"
+         btnCalcular.setOnClickListener {
+             val calculoIntent =  Intent(this, MainCalculo::class.java)
+             startActivity(calculoIntent);
+         }
+
+        val btnNovoMes: Button = findViewById(R.id.btnNovoMes)
+        btnNovoMes.setOnClickListener {
+            val novomesIntent =  Intent(this, MainNovoMes::class.java)
+            startActivity(novomesIntent);
+         }
+
+        val btnConfig: Button = findViewById(R.id.btnConfig)
+        btnConfig.setOnClickListener {
+            val configIntent =  Intent(this, MainConfig::class.java)
+            startActivity(configIntent);
+        }
+
+        val btnSair: Button = findViewById(R.id.btnSair)
+        btnSair.setOnClickListener {
+            AlertDialog.Builder(this)
+                    .setTitle("Notification")
+                    .setMessage("Finalizando")
+                    .setPositiveButton("Ok", { dialog, which -> }).show()
+            finish()
+        }
         // https://codelabs.developers.google.com/codelabs/build-your-first-android-app-kotlin/index.html?index=..%2F..%2Findex#7
     }
 
-        fun jurosCalcular (view: View) {
+    /**
+     fun jurosCalcular (view: View) {
             val calculoIntent =  Intent(this, MainCalculo::class.java)
-
-            // Get the current value of the text view.
-            //val countString = textView.text.toString()
-
-            // Convert the count to an int
-            //val count = Integer.parseInt(countString)
-
-            // Add the count to the extras for the Intent.
-            //randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
-
-            // Start the new activity.
-            //calculoIntent.setFlag(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-            // Start an activity if it's safe
-
             startActivity(calculoIntent);
-
-        }
+      }
 
     fun jurosNovoMes (view: View) {
         val novomesIntent =  Intent(this, MainNovoMes::class.java)
-
         startActivity(novomesIntent);
-
     }
 
     fun jurosConfig (view: View) {
         val configIntent =  Intent(this, MainConfig::class.java)
-
         startActivity(configIntent);
-
     }
 
     fun jurosSair (view: View) {
-
         AlertDialog.Builder(this)
                 .setTitle("Notification")
                 .setMessage("Finalizando")
                 .setPositiveButton("Ok", { dialog, which -> }).show()
-
        finish()
 
     }
+     */
 }
