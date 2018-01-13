5c69be2ff2c214edcb643763e835730ac662ac2e
fcoedn <fcoedn@gmail.com>
2018-01-13 11:29:46 -0300

    Casa 2000

--- app/src/main/java/com/example/francisco/calculojurospagamento/MainMenu.kt
+++ app/src/main/java/com/example/francisco/calculojurospagamento/MainMenu.kt
@@ -1,19 +1,26 @@
 package com.example.francisco.calculojurospagamento
 
 import android.content.Intent
+import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 
 class MainMenu : AppCompatActivity()   {
 
+    lateinit var usersDBHelper : UsersDBHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        usersDBHelper = UsersDBHelper(this)
 
         val btnCalcular: Button = findViewById(R.id.btnCalcular)
         btnCalcular.text = "Calcular"
@@ -23,10 +30,10 @@ class MainMenu : AppCompatActivity()   {
          }
 
         val btnNovoMes: Button = findViewById(R.id.btnNovoMes)
-        btnNovoMes.setOnClickListener {
-            val novomesIntent =  Intent(this, MainNovoMes::class.java)
-            startActivity(novomesIntent);
-         }
+       // btnNovoMes.setOnClickListener {
+        //    val novomesIntent =  Intent(this, MainNovoMes::class.java)
+        //    startActivity(novomesIntent);
+        // }
 
         val btnConfig: Button = findViewById(R.id.btnConfig)
         btnConfig.setOnClickListener {
@@ -45,6 +52,17 @@ class MainMenu : AppCompatActivity()   {
         // https://codelabs.developers.google.com/codelabs/build-your-first-android-app-kotlin/index.html?index=..%2F..%2Findex#7
     }
 
+    fun addUser(v:View){
+        //val x: EditText = findViewById(R.id.edittext_name.)
+        var codigo = this.edittext_userid.text.toString()
+        var nome = this.edittext_name.text.toString()
+     //   var result = usersDBHelper.insertUser(UserModel(codigo = codigo,nome = nome))
+        //clear all edittext s
+    //    this.edittext_name.setText("")
+     //   this.edittext_userid.setText("")
+     //   this.textview_result.text = "Added user : "+result
+     //   this.ll_entries.removeAllViews()
+    }
     /**
      fun jurosCalcular (view: View) {
             val calculoIntent =  Intent(this, MainCalculo::class.java)
@@ -71,3 +89,4 @@ class MainMenu : AppCompatActivity()   {
     }
      */
 }
+
